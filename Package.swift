a7f5af912ac8d1808e7aec4e558677ac5b2e3e34
Raymond <raymondxxu>
2022-11-12 15:35:55 -0800

    Add Dependency, update git ignore

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "Proxy",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/raymondxxu/Comp7005FinalProjectCommonLib", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Proxy",
-            dependencies: []),
+            dependencies: [
+                .product(name: "CommonLib", package: "Comp7005FinalProjectCommonLib")
+            ]),
         .testTarget(
             name: "ProxyTests",
             dependencies: ["Proxy"]),
