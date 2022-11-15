25e155db3d259f608bbc1602f4cda2bd6172d9ef
Raymond <raymondxxu>
2022-11-14 21:23:26 -0800

    Update proxy

--- Sources/Proxy/Proxy.swift
+++ Sources/Proxy/Proxy.swift
@@ -1,8 +1,30 @@
+import Socket
+import CommonLib
+import Foundation
+
 @main
 public struct Proxy {
-    public private(set) var text = "Hello, World!"
 
     public static func main() {
-        print(Proxy().text)
+        let argParser = ArgParser.proxyArgParser
+        do {
+            try argParser.parse()
+        } catch(let err) {
+            print(err)
+        }
+        let portNumber: UInt16 = argParser.portNumber ?? 2222
+        let socketManger = SocketManager(isForServer: false,
+                                         serverIP: "",
+                                         port: portNumber)
+
+        let senderIp = argParser.senderIp
+        let receiverIp = argParser.receiverIp
+        var fromFD: CInt = 0
+        var toFD: CInt = 0
+        do {
+            try socketManger.netWorkSnake(from: senderIp!, to: receiverIp!)
+        } catch (let err ) {
+            print(err)
+        }
     }
 }
